95975eca4525f6912835ef875533a1aa71e8ade8
LuMiSxh <schmidt.lucamichael@icloud.com>
2022-12-05 20:05:16 +0100

    Adding a cache for cargo (Should speed up the workflow by about 4x) and making deploy its own job

--- akemi/svelte.config.js
+++ akemi/svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 			precompress: false
 		}),
 		paths: {
-			// base: process.env.NODE_ENV === 'production' ? '/tomoakemi' : '',
+			base: process.env.NODE_ENV === 'production' ? '/tomoakemi' : '',
 		}
 	}
 };
